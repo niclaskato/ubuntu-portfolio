11b42c148187a1a052a9ea24c4f1d95133904b01
Nicholas Kato <87049471+niclaskato@users.noreply.github.com>
2022-12-10 01:58:50 +0900

    styling star button

--- components/apps/niclas.js
+++ components/apps/niclas.js
@@ -95,7 +95,7 @@ export class AboutNiclas extends Component {
                     <img className="w-5 md:w-5" alt="nicholas's projects" src="https://img.icons8.com/color/344/project.png" />
                     <span className="ml-1 md:ml-2 text-gray-50 text-lg">Projects</span>
                 </div>
-                <div className='my-0.5 w-28 md:w-full h-8 px-2 md:px-2.5 flex justify-center items-center'>
+                <div className='my-0.5 mt-4 w-28 md:w-full h-8 px-2 md:px-2.5 flex justify-center items-center'>
                     <a class="flex justify-center items-center gap-1.5 w-4/5 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full" href="https://github.com/niclaskato/ubuntu-portfolio" target="_blank">
                       Star <img className="w-5 md:w-5" alt="nicholas's projects" src="https://img.icons8.com/officel/512/star.png" />
                     </a>
